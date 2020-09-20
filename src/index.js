3a95dc0e9e0dc9213519adc94878e83240500393
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-20 22:31:57 +0300

    update app

--- src/index.js
+++ src/index.js
@@ -6,22 +6,67 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    const math = require('mathjs');
-    try {
-        const result = math.evaluate(expr);
-        if (!Number.isFinite(result) || expr.includes('/ 0')) {
-            throw new Error("TypeError: Division by zero.");
+    // split expression by operator considering parentheses
+    const split = (expression, operator) => {
+        const result = [];
+        let braces = 0;
+        let currentChunk = "";
+        for (let i = 0; i < expression.length; ++i) {
+            const curCh = expression[i];
+            if (curCh == '(') {
+                braces++;
+            } else if (curCh == ')') {
+                braces--;
+            }
+            if (braces == 0 && operator == curCh) {
+                result.push(currentChunk);
+                currentChunk = "";
+            } else currentChunk += curCh;
+        }
+        if (currentChunk != "") {
+            result.push(currentChunk);
         }
         return result;
-    } catch (e) {
-        if (e instanceof SyntaxError) {
-            throw new Error("ExpressionError: Brackets must be paired");
-        }
+    };
+    // this will only take strings containing * operator [ no + ]
+    const parseMultiplicationSeparatedExpression = (expression) => {
+        const numbersString = split(expression, '*');
+        const numbers = numbersString.map(noStr => {
+            if (noStr[0] == '(') {
+                const expr = noStr.substr(1, noStr.length - 2);
+                // recursive call to the main function
+                return parsePlusSeparatedExpression(expr);
+            }
+            return +noStr;
+        });
+        const initialValue = 1.0;
+        const result = numbers.reduce((acc, no) => acc * no, initialValue);
+        return result;
+    };
+    // both * -
+    const parseMinusSeparatedExpression = (expression) => {
+        const numbersString = split(expression, '-');
+        const numbers = numbersString.map(noStr => parseMultiplicationSeparatedExpression(noStr));
+        const initialValue = numbers[0];
+        const result = numbers.slice(1).reduce((acc, no) => acc - no, initialValue);
+        return result;
+    };
+    // * - +
+    const parsePlusSeparatedExpression = (expression) => {
+        const numbersString = split(expression, '+');
+        const numbers = numbersString.map(noStr => parseMinusSeparatedExpression(noStr));
+        const initialValue = 0.0;
+        const result = numbers.reduce((acc, no) => acc + no, initialValue);
+        return result;
+    };
+    const parse = (expression) => {
 
-        if (e.toString() === "Error: TypeError: Division by zero.") {
-            throw new Error("TypeError: Division by zero.");
-        }
-    }
+        return parsePlusSeparatedExpression(expression, '+');
+
+    };
+    console.log(expr);
+    console.log(expr.split('').filter((i) => i !== ' ').join(''));
+    return parse(expr.split('').filter((i) => i !== ' ').join(''));
 }
 
 module.exports = {
