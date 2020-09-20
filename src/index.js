ed7632ac52e4c8e34c4c4c1a5524d9ded1f4ad02
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-20 21:40:55 +0300

    app: created function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+
+
 function eval() {
     // Do not use eval!!!
     return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const math = require('mathjs');
+    try {
+        const result = math.evaluate(expr);
+        if (!Number.isFinite(result) || expr.includes('/ 0')) {
+            throw new Error("TypeError: Division by zero.");
+        }
+        return result;
+    } catch (e) {
+        if (e instanceof SyntaxError) {
+            throw new Error("ExpressionError: Brackets must be paired");
+        }
+
+        if (e.toString() === "Error: TypeError: Division by zero.") {
+            throw new Error("TypeError: Division by zero.");
+        }
+    }
 }
 
 module.exports = {
