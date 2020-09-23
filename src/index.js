273640c35a794fc0cc064107d5e44b8bf48a228d
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-23 21:11:46 +0300

    app: update js

--- src/index.js
+++ src/index.js
@@ -5,68 +5,92 @@ function eval() {
     return;
 }
 
+function substraction(array) {
+    let value = array.filter((i) => i !== '-');
+    value = value.reduce((acc, i, index, arr) => {
+        if (index === 0) {
+            acc = parseFloat(i);
+            return acc;
+        }
+        return (acc - parseFloat(i));
+    }, 0);
+    return value;
+}
+
+function addition(array) {
+    let value = array.filter((i) => i !== '+');
+    value = value.reduce((acc, i) => acc + parseFloat(i), 0);
+    return value;
+}
+
+function multiplicationAndDivision(array) {
+    const returnValue = array.map((i, index, arr) => {
+        if (index > 0 && (arr[index - 1] === '/' || arr[index - 1] === '*')) {
+            const value = arr[index - 1] === '*' ? arr[index - 2] * arr[index] : arr[index - 2] / arr[index];
+            arr[index] = value;
+            return value;
+        }
+        return i;
+    });
+    return returnValue[returnValue.length - 1];
+}
+
+function dispatcher(str) {
+    const object = {
+        '-': (array) => substraction(array),
+        '*': (array) => multiplicationAndDivision(array),
+        '/': (array) => multiplicationAndDivision(array),
+    }
+    console.log(`str: ${str === '-'}`);
+    return object[str];
+}
+
+function makeAnArrayFromString(str) {
+    return str.split(' ').join('').split('').map((i) => {
+        if (i === '*' || i === '/' || i === '-' || i === '+') {
+            return ` ${i} `;
+        }
+        return i
+    }).join('').split(' ');
+};
+
+function stringAnalyser(str) {
+    let returnArray = [];
+    const operands = ['-', '*', '/'];
+    for (let i = 0; i < operands.length; i += 1) {
+        if (str.includes(operands[i]) && !returnArray.includes(operands[i])) {
+            returnArray.push(operands[i]);
+        }
+    }
+    return returnArray.map((i) => {
+        if (i === '/' && returnArray.includes('*')) {
+            return '';
+        }
+        return i;
+    }).filter((i) => i !== '');
+}
+
 function expressionCalculator(expr) {
-    // split expression by operator considering parentheses
-    const split = (expression, operator) => {
-        const result = [];
-        let braces = 0;
-        let currentChunk = "";
-        for (let i = 0; i < expression.length; ++i) {
-            const curCh = expression[i];
-            if (curCh == '(') {
-                braces++;
-            } else if (curCh == ')') {
-                braces--;
-            }
-            if (braces == 0 && operator == curCh) {
-                result.push(currentChunk);
-                currentChunk = "";
-            } else currentChunk += curCh;
-        }
-        if (currentChunk != "") {
-            result.push(currentChunk);
-        }
-        return result;
-    };
-    // this will only take strings containing * operator [ no + ]
-    const parseMultiplicationSeparatedExpression = (expression) => {
-        const numbersString = split(expression, '*');
-        const numbers = numbersString.map(noStr => {
-            if (noStr[0] == '(') {
-                const expr = noStr.substr(1, noStr.length - 2);
-                // recursive call to the main function
-                return parsePlusSeparatedExpression(expr);
-            }
-            return +noStr;
-        });
-        const initialValue = 1.0;
-        const result = numbers.reduce((acc, no) => acc * no, initialValue);
-        return result;
-    };
-    // both * -
-    const parseMinusSeparatedExpression = (expression) => {
-        const numbersString = split(expression, '-');
-        const numbers = numbersString.map(noStr => parseMultiplicationSeparatedExpression(noStr));
-        const initialValue = numbers[0];
-        const result = numbers.slice(1).reduce((acc, no) => acc - no, initialValue);
-        return result;
-    };
-    // * - +
-    const parsePlusSeparatedExpression = (expression) => {
-        const numbersString = split(expression, '+');
-        const numbers = numbersString.map(noStr => parseMinusSeparatedExpression(noStr));
-        const initialValue = 0.0;
-        const result = numbers.reduce((acc, no) => acc + no, initialValue);
-        return result;
-    };
-    const parse = (expression) => {
 
-        return parsePlusSeparatedExpression(expression, '+');
-
-    };
-    console.log(expr);
-    console.log(expr.split('').filter((i) => i !== ' ').join(''));
-    return parse(expr.split('').filter((i) => i !== ' ').join(''));
+    if (expr.includes('/ 0')) throw new Error("TypeError: Division by zero.");
+    const newExp = makeAnArrayFromString(expr);
+    const arrayOutOfPlus = newExp.join('').split('+');
+    let aa = arrayOutOfPlus.reduce((acc, i) => {
+        if (stringAnalyser(i).length === 1) {
+            return acc + dispatcher(stringAnalyser(i)[0])(makeAnArrayFromString(i));
+        }
+        else if (stringAnalyser(i).length > 1) {
+            return acc + substraction(i.split('-').map((i) => {
+                if (stringAnalyser(i).length === 1) {
+                    return multiplicationAndDivision(makeAnArrayFromString(i));
+                }
+                return i;
+            }));
+        } else {
+            return acc + parseFloat(i);
+        }
+    }, 0);
+    return aa;
 }
 
 module.exports = {
