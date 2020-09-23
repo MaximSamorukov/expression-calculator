1b17fe309d6210d7088e2a7b3da633b5ba9e1487
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-23 23:17:05 +0300

    app: update js

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ function addition(array) {
 }
 
 function multiplicationAndDivision(array) {
+    // console.log(array);
     const returnValue = array.map((i, index, arr) => {
         if (index > 0 && (arr[index - 1] === '/' || arr[index - 1] === '*')) {
             const value = arr[index - 1] === '*' ? arr[index - 2] * arr[index] : arr[index - 2] / arr[index];
@@ -32,6 +33,7 @@ function multiplicationAndDivision(array) {
         }
         return i;
     });
+    // console.log(returnValue[returnValue.length - 1]);
     return returnValue[returnValue.length - 1];
 }
 
@@ -46,14 +48,16 @@ function dispatcher(str) {
 
 function makeAnArrayFromString(str) {
     return str.split(' ').join('').split('').map((i) => {
-        if (i === '*' || i === '/' || i === '-' || i === '+') {
+        if (i === '*' || i === '/' || i === '-' || i === '+' || i === ')' || i === '(') {
             return ` ${i} `;
         }
         return i
-    }).join('').split(' ');
+    }).join('').split(' ').filter((i) => i !== '');
 };
 
 function stringAnalyser(str) {
+    console.log(str);
+    console.log(typeof str);
     let returnArray = [];
     const operands = ['-', '*', '/'];
     for (let i = 0; i < operands.length; i += 1) {
@@ -69,13 +73,52 @@ function stringAnalyser(str) {
     }).filter((i) => i !== '');
 }
 
+function bracketSolver(arr) {
+    const innerBracketExpression = [];
+    const originalExpression = []
+    let count = 0;
+    arr.map((i, index, arr) => {
+        if (i === '(') {
+            count = count + 1;
+        };
+        if (i === ')') {
+            count = count - 1;
+            if (count === 0) {
+                originalExpression.push(expressionCalculator(innerBracketExpression.join('')));
+            };
+        };
+        if (count > 0 && i !== '(') {
+            innerBracketExpression.push(i);
+        };
+        if (count === 0 && i !== ')') {
+            originalExpression.push(i);
+        };
+        if (count < 0 || (count > 0 && index === arr.length - 1)) {
+            throw new Error("ExpressionError: Brackets must be paired");
+        };
+        return i;
+    })
+    return originalExpression;
+}
+
 function expressionCalculator(expr) {
 
-    if (expr.includes('/ 0')) throw new Error("TypeError: Division by zero.");
+    if (expr.includes('/ 0') || expr.includes('/0')) throw new Error("TypeError: Division by zero.");
     const newExp = makeAnArrayFromString(expr);
-    const arrayOutOfPlus = newExp.join('').split('+');
+    // console.log(newExp);
+    const newExpresWithoutBrackets = bracketSolver(newExp);
+    // console.log(newExpresWithoutBrackets);
+    const arrayOutOfPlus = newExpresWithoutBrackets.join('').split('+');
+    // console.log(arrayOutOfPlus);
     let aa = arrayOutOfPlus.reduce((acc, i) => {
+        // console.log(`acc: ${acc}`);
+        // console.log(`i: ${i}`);
+        // console.log(stringAnalyser(i));
         if (stringAnalyser(i).length === 1) {
+            // console.log(`i: ${i}`);
+            // console.log((makeAnArrayFromString(i)));
+            // console.log((stringAnalyser(i)));
+            // console.log(dispatcher(stringAnalyser(i)[0])(makeAnArrayFromString(i)));
             return acc + dispatcher(stringAnalyser(i)[0])(makeAnArrayFromString(i));
         }
         else if (stringAnalyser(i).length > 1) {
@@ -89,6 +132,7 @@ function expressionCalculator(expr) {
             return acc + parseFloat(i);
         }
     }, 0);
+    // console.log(aa);
     return aa;
 }
 
